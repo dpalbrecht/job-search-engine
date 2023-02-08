3f111c3e3fdfa14b13980df59f3d2f4542f4908e
David Albrecht <davidpabloalbrecht@gmail.com>
2023-02-08 08:42:18 -0800

    add json format

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 
-# Title and search bar
+# Title and search bar, and format options
 st.markdown('<h1>Search Relevance & Matching Tech</h1><br>', unsafe_allow_html=True)
 col1, col2, _ = st.columns([1,2,1])
 with col1:
@@ -14,17 +14,21 @@ with col2:
     query = st.text_input(label="Find jobs...",
                           placeholder=f"Search through {search_index.count():,} jobs...",
                           label_visibility='collapsed')
+    json_flag = st.checkbox(label='JSON Format')
+st.markdown('<hr>', unsafe_allow_html=True)
 
 
 # Show query results
 query_results = search_index.query(query)
-for result in query_results['hits']['hits']:
-    days_ago_posted = (datetime.utcnow() - datetime.strptime(result['_source']['created_at'], '%Y-%m-%d')).days
-    st.markdown(f"""
-    <hr>
-    <h3>{result['_source']['company']}</h3>
-    <h5><a href={result['_source']['url']}>{result['_source']['title']}</a></h5>
-    <div style="padding:0px 0px 16px;"><b>{result['_source']['poster']} posted {days_ago_posted} days ago</b></div>
-    <div>{result['_source']['description'][:1000]+'...'}</div>
-    """, unsafe_allow_html=True)
-st.markdown('<hr>', unsafe_allow_html=True)
+if json_flag:
+    st.json(query_results['hits']['hits'])
+else:
+    for result in query_results['hits']['hits']:
+        days_ago_posted = (datetime.utcnow() - datetime.strptime(result['_source']['created_at'], '%Y-%m-%d')).days
+        st.markdown(f"""
+        <h3>{result['_source']['company']}</h3>
+        <h5><a href={result['_source']['url']}>{result['_source']['title']}</a></h5>
+        <div style="padding:0px 0px 16px;"><b>{result['_source']['poster']} posted {days_ago_posted} days ago</b></div>
+        <div>{result['_source']['description'][:1000]+'...'}</div>
+        <hr>
+        """, unsafe_allow_html=True)
