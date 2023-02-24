04bbaaf2becdfa34f6907735bbcaf84e3be49596
Doug Turnbull <softwaredoug@gmail.com>
2023-02-24 16:11:11 -0500

    Add basic crawling of linkedin

--- pages/1_Post_a_Job.py
+++ pages/1_Post_a_Job.py
@@ -1,9 +1,43 @@
 import streamlit as st
 import sys
+import json
 sys.path.append('..')
 import search_index
 import css; css.set_page_style()
 from datetime import datetime
+from bs4 import BeautifulSoup
+import re
+import requests
+
+def crawl(url):
+    # From https://colab.research.google.com/drive/1L_s0ey6T-aK65J2wHSZEhoBVW8vmJlkH?usp=sharing#scrollTo=a8yeMg5Tv9Nx
+    html = requests.get(url).content
+    soup = BeautifulSoup(html)
+    description = soup.find(
+        name='div',
+        attrs={'class':"show-more-less-html__markup show-more-less-html__markup--clamp-after-5"}
+    )
+    description = description.get_text('\n')
+    # company_info = json.loads(soup.find(name='script', attrs={'type':'application/ld+json'}).text)
+    title = soup.find('h1').text
+    company = soup.find('a', {'href': re.compile('linkedin.com/company/*')}).text
+
+    return {
+        "url": url,
+        "company": company,
+        "title": title,
+        "poster": "Unknown",
+        "description": description,
+    }
+
+
+def crawl_and_populate():
+    url = st.session_state.crawl_url
+    crawled = crawl(url)
+    st.session_state.url = crawled['url']
+    st.session_state.title = crawled['title']
+    st.session_state.company = crawled['company']
+    st.session_state.description = crawled['description']
 
 
 
@@ -11,12 +45,23 @@ from datetime import datetime
 st.markdown('<h1>Post a Job</h1><br>', unsafe_allow_html=True)
 _, col2, _ = st.columns([1,8,1])
 with col2:
+    with st.form("crawl_form"):
+        st.markdown("Attempt to populate by crawling job URL (beta)")
+        st.text_input(label='URL', placeholder='URL *',
+                      label_visibility='collapsed',
+                      key='crawl_url')
+        submitted = st.form_submit_button("Crawl (Beta)", on_click=crawl_and_populate)
     with st.form("job_form"):
+        st.markdown("Edit / confirm your submission")
         payload = {
-            'company': st.text_input(label='Company', placeholder='Company *', label_visibility='collapsed'),
-            'title': st.text_input(label='Job Title', placeholder='Job Title *', label_visibility='collapsed'),
-            'description': st.text_area(label='Description', placeholder='Description *', label_visibility='collapsed'),
-            'url': st.text_input(label='URL', placeholder='URL *', label_visibility='collapsed'),
+            'company': st.text_input(label='Company', key='company',
+                                     placeholder='Company *', label_visibility='collapsed'),
+            'title': st.text_input(label='Job Title', key='title',
+                                   placeholder='Job Title *', label_visibility='collapsed'),
+            'description': st.text_area(label='Description', placeholder='Description *',
+                                        label_visibility='collapsed', key='description'),
+            'url': st.text_input(label='URL', placeholder='URL *', label_visibility='collapsed',
+                                 key='url'),
             'poster': st.text_input(label='Your Name', placeholder='Your Name', label_visibility='collapsed'),
             'eu': st.checkbox(label='EU')
         }
@@ -27,7 +72,7 @@ with col2:
 if submitted:
     post_payload = True
     for name, value in payload.items():
-        if (name not in ['poster','EU']) and (value == ''):
+        if (name not in ['poster', 'EU']) and (value == ''):
             post_payload = False
             st.warning(f"The '{name}' parameter is required. Can't post job.", icon='🚨')
     if post_payload:
