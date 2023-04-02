c42eebf2275538d775d58bf98ac4911bd03fed1a
David Albrecht <davidpabloalbrecht@gmail.com>
2023-04-01 21:52:17 -0700

    add find similar jobs

--- Home.py
+++ Home.py
@@ -10,6 +10,8 @@ s3 = boto3.resource('s3')
 
 
 # Title and search bar, and format options
+if 'query' not in st.session_state:
+    st.session_state.query = ''
 st.markdown('<h1>Search Relevance & Matching Tech</h1>', unsafe_allow_html=True)
 st.markdown("""<h4><a href="https://www.opensourceconnections.com/slack/">Join the Slack Channel</a></h4><br><br>""", unsafe_allow_html=True)
 col1, col2, _ = st.columns([1,2,1])
@@ -17,7 +19,8 @@ with col1:
     st.markdown('<h3 style="text-align:end; padding:0px;">&#x1F50D;&#xFE0D;</h3>', unsafe_allow_html=True)
 with col2:
     query = st.text_input(label="Find jobs...",
-                          placeholder="Search through jobs...",
+                          value=st.session_state.query,
+                          placeholder='Search through jobs...',
                           label_visibility='collapsed')
     most_recent_flag = st.checkbox(label='Last 30 Days', value=True)
     eu_flag = st.checkbox(label='EU')
@@ -33,6 +36,11 @@ def click_job_url(url, query, rank):
     s3object.put(
         Body=(bytes(json.dumps({'query':query, 'url':url, 'rank':rank}).encode('UTF-8')))
     )
+
+
+# Update session query from Find Similar Jobs
+def update_session_query(new_query):
+    st.session_state.query = new_query
 
 
 # Show query results
@@ -54,6 +62,10 @@ else:
                   key=result['_source']['url'],
                   on_click=click_job_url,
                   kwargs={'url':result['_source']['url'], 'query':query, 'rank':n})
+        st.button('Find Similar Jobs',
+                  key=result['_source']['url']+'_FIND_SIMILAR_JOBS',
+                  on_click=update_session_query,
+                  kwargs={'new_query':result['_source']['title']})
         st.markdown(f"""
         <div style="padding:0px 0px 16px;"><b>{poster_msg}</b></div>
         <div>{result['_source']['description'][:1000]+'...'}</div>
