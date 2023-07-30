68d00e67fe25732ec486079e144d53b408cbca5c
David Albrecht <davidpabloalbrecht@gmail.com>
2023-07-29 21:08:23 -0700

    add page events

--- google_analytics.py
+++ google_analytics.py
@@ -4,6 +4,17 @@ import pathlib
 from bs4 import BeautifulSoup
 import shutil
 import streamlit as st
+from collections import defaultdict
+from google.oauth2 import service_account
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.analytics.data_v1beta.types import (
+    DateRange,
+    Dimension,
+    Metric,
+    RunReportRequest,
+    FilterExpression,
+    Filter
+)
 
 
 
@@ -34,3 +45,39 @@ def inject_google_analytics():
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_JS)
         index_path.write_text(new_html)
+
+
+def get_data(query):
+    credentials = service_account.Credentials.from_service_account_file(
+        'google_analytics_credentials.json',
+        scopes=["https://www.googleapis.com/auth/analytics.readonly"],
+    )
+    client = BetaAnalyticsDataClient(credentials=credentials)
+    response = client.run_report(query)
+    return response.rows
+
+
+def parse_page_events(response):
+    data = defaultdict(lambda: defaultdict(lambda: 0))
+    for row in response:
+        event_name, page_title = row.dimension_values
+        event_count = row.metric_values[0]
+        data[event_name.value][page_title.value] += int(event_count.value)
+    return data
+
+
+def query_google_analytics(report_type, start_date, end_date):
+    if report_type == 'page_events':
+        query = RunReportRequest(
+            property=f"properties/{os.environ['GA4_PROPERTY_ID']}",
+            dimensions=[Dimension(name="eventName"), Dimension(name="pageTitle")],
+            metrics=[Metric(name="eventCount")],
+            date_ranges=[DateRange(start_date=start_date, end_date=end_date)],
+            dimension_filter=FilterExpression(
+                filter=Filter(
+                    field_name="eventName",
+                    in_list_filter=Filter.InListFilter(values=["page_view","click"])
+                )
+            )
+        )
+        return parse_page_events(get_data(query))
